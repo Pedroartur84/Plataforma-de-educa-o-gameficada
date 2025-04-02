e3fddaf2694ddb369993e058121832f3ddd190ac
Pedro artur felix de melo <pedroarturfelixdemelo@gmail.com>
2025-04-02 20:34:13 +0000

    models de usuario, feito

--- usuarios/models.py
+++ usuarios/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class usuario(AbstractUser):
+    tipo_usuario = models.charfiel(
+        max_length=20,
+        choices=[
+            ('aluno', 'aluno'),
+            ('profesor','profesor'),
+            ('admin', 'admin')
+        ]
+        choices=tipos_usuario,
+        default='aluno'
+        #aluno sera o valor padrão
+    )
