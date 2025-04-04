b5de64c3f17fb5dd0c17b68e53e685304d59d7f3
pedroartu84 <pedroarturfelixdemelo>
2025-04-04 09:00:18 -0300

    Atualizações nos modelos e remoção do template cadastro.html

--- usuarios/models.py
+++ usuarios/models.py
@@ -2,15 +2,15 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
-class usuario(AbstractUser):
-    tipo_usuario = models.charfiel(
+class Usuario(AbstractUser):
+    tipo_usuario = models.CharField(
         max_length=20,
         choices=[
-            ('aluno', 'aluno'),
-            ('profesor','profesor'),
-            ('admin', 'admin')
-        ]
-        choices=tipos_usuario,
+            ('aluno', 'Aluno'),
+            ('professor', 'Professor'),
+            ('admin', 'Admin')
+        ],
         default='aluno'
-        #aluno sera o valor padrão
     )
+    
+    
