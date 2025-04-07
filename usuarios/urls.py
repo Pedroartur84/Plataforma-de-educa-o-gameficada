5a53b61db959af6c9a2ebae8d48f7cf69bfc0fd1
pedroartu84 <pedroarturfelixdemelo>
2025-04-07 08:39:23 -0300

    tentando fazer os templates rodarem

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -2,6 +2,6 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('home', views.home, name='home'),
-    path('cadastro/', views.cadastro, name='cadastro')
+    path('', views.home, name='home'), #pagina inicial
+    path('cadastro/', views.cadastro, name='cadastro') #pagina de cadastro
 ]
