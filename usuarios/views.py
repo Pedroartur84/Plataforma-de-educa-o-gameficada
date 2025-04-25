216b28c4b54e467e5a3c8102e4c7a29c7e883aee
pedroartu84 <pedroarturfelixdemelo>
2025-04-25 08:29:51 -0300

    adicionado algums comentarios para menhor entendimento.

--- usuarios/views.py
+++ usuarios/views.py
@@ -4,15 +4,21 @@ from django.shortcuts import render, redirect
 from .forms import LoginForm, CadastroForm
 
 def login_view(request):
+    # verifica se a requisição é do tipo POST
     if request.method == "POST":
         form = LoginForm(request, data=request.POST)
+        # verifica se o formulario é válido
         if form.is_valid():
+            # tenta autenticar o usuario com as credencias fornecidas
             user = authenticate(
                 username=form.cleaned_data['username'],  # O email está aqui
                 password=form.cleaned_data['password']
             )
+            # se o usuario foi autenticado com sucesso
             if user is not None:
+                # faz o login do usuario
                 login(request, user)
+                # verifica se o usuário marcou "lembrarme"
                 if not form.cleaned_data.get('remember_me'):
                     request.session.set_expiry(0)  # Sessão do navegador
                 return redirect('home')
@@ -25,11 +31,14 @@ def login_view(request):
     
     return render(request, 'login.html', {'form': form})
 
-
+# view para cadastro
 def cadastro(request):
+    # verifica se a requisicao e do tipo POST
     if request.method == 'POST':
         form = CadastroForm(request.POST)
+        # verifica se o formulario e valido
         if form.is_valid():
+            # salva o novo usuario no banco de dados
             user = form.save()
             messages.success(request, 'Cadastro realizado com sucesso! Faça login.')
             return redirect('login')
