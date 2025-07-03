d54a83d521391fe3899e81dcc8c4acdc482f9a90
pedroartu84 <pedroarturfelixdemelo>
2025-07-03 16:08:26 -0300

    views para deixar o modal funcional

--- usuarios/views.py
+++ usuarios/views.py
@@ -2,6 +2,7 @@ from django.contrib import messages
 from django.contrib.auth import login, authenticate
 from django.shortcuts import render, redirect
 from .forms import LoginForm, CadastroForm
+from .models import *
 
 def login_view(request):
     # verifica se a requisição é do tipo POST
@@ -48,5 +49,21 @@ def cadastro(request):
     
     return render(request, 'cadastro/cadastrar.html', {'form': form})
 
+
 def principal(request):
-    return render(request, 'principal/principal_page.html')
+    salas = Sala.objects.all().order_by('-data_criacao')
+    return render(request, 'principal/principal_page.html', {'salas': salas})
+
+
+def criar_sala(request):
+    if request.method == 'POST':
+        nome = request.POST.get('nome_sala')
+        descricao = request.POST.get('descricao')
+        Sala.objects.create(nome=nome, descricao=descricao, criador=request.user)
+        return  redirect('usuarios:pag_principal')
+    return  redirect('usuarios:pag_principal')
+
+
+def listar_salas(request):
+    salas = Sala.objects.all().order_by('-data_criacao')
+    return render(request, 'principal/principal_page.html', {'salas': salas})
