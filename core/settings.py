a32c00a70d87ab2302bf91328aed8cea8326ae66
Pedroartur84 <pedroarturfelixdemelo@gmail.com>
2026-02-06 11:01:55 -0300

    Porta 587 (SMTP) bloqueada no Render free. Solução: Usar API do SendGrid via HTTPS/443 (aberta).

--- core/settings.py
+++ core/settings.py
@@ -107,16 +107,12 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Seu modelo de usuário customizado
 AUTH_USER_MODEL = 'usuarios.Usuario'
 
-# Configuração de email usando SMTP do SendGrid
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.sendgrid.net'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'apikey'  # usuário fixo para autenticação SMTP com API Key
-EMAIL_HOST_PASSWORD = os.environ.get('SENDGRID_API_KEY')
+# Configuração de email usando API do SendGrid (HTTPS/443 - funciona no Render free)
+EMAIL_BACKEND = 'sendgrid_backend.SendgridBackend'
+SENDGRID_API_KEY = os.environ.get('SENDGRID_API_KEY')
 DEFAULT_FROM_EMAIL = 'contatoplayer040@gmail.com'
 
-# Flags úteis para lógica de verificação por email (sem django-allauth, mas úteis)
+# Flags úteis para lógica de verificação por email
 ACCOUNT_EMAIL_REQUIRED = True
 ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
 ACCOUNT_AUTHENTICATION_METHOD = 'email'
