fcd82abb11b937f2a0c7047fe50089e9cda38d52
Pedroartur84 <pedroarturfelixdemelo@gmail.com>
2026-02-20 13:11:37 -0300

    tentando trasformar em pwa2

--- core/views.py
+++ core/views.py
@@ -7,5 +7,15 @@ def home(request):
     return render(request,'home.html')
 
 def service_worker(request):
+    """
+    Serve o service worker na raiz do domínio (/sw.js).
+    Isso é OBRIGATÓRIO para que o SW tenha escopo sobre todas as páginas.
+    O arquivo deve estar em static/js/sw.js.
+    """
     sw_path = os.path.join(settings.BASE_DIR, 'static', 'js', 'sw.js')
-    return FileResponse(open(sw_path, 'rb'), content_type='application/javascript')
+    response = FileResponse(open(sw_path, 'rb'), content_type='application/javascript')
+    # O header Service-Worker-Allowed expande o escopo se necessário
+    response['Service-Worker-Allowed'] = '/'
+    # Não cachear o próprio SW para garantir atualizações
+    response['Cache-Control'] = 'no-cache, no-store, must-revalidate'
+    return response
